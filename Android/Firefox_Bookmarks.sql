fb530c4df0cb85f73531e094bec70bb3cb9d5113
Kevin - Stark 4N6 <48143894+stark4n6@users.noreply.github.com>
2022-01-17 15:13:25 -0500

    Update Firefox_Bookmarks.sql

--- Android/Firefox_Bookmarks.sql
+++ Android/Firefox_Bookmarks.sql
@@ -1,17 +1,17 @@
 SELECT
-datetime(moz_bookmarks.dateAdded/1000,'unixepoch'),
-datetime(moz_bookmarks.lastModified/1000,'unixepoch'),
-moz_bookmarks.title,
-moz_places.url,
+datetime(moz_bookmarks.dateAdded/1000,'unixepoch') AS 'DateAdded',
+datetime(moz_bookmarks.lastModified/1000,'unixepoch') AS 'DateLastModified',
+moz_bookmarks.title AS 'Title',
+moz_places.url AS 'URL',
 CASE moz_bookmarks.type
     WHEN 1 THEN 'URL'
     WHEN 2 THEN 'Folder'
     WHEN 3 THEN 'Separator'
-END,
-moz_bookmarks.id,
-moz_bookmarks.parent,
-moz_bookmarks.position,
-moz_bookmarks.syncStatus
+END 'Type',
+moz_bookmarks.id AS 'ID',
+moz_bookmarks.parent AS 'ParentID',
+moz_bookmarks.position AS 'Position',
+moz_bookmarks.syncStatus AS 'SyncStatus'
 FROM moz_bookmarks
 LEFT JOIN moz_places ON moz_bookmarks.fk = moz_places.id
 ORDER BY moz_bookmarks.id ASC
